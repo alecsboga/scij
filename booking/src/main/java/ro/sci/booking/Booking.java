f93dfc5a1763f91e7a06005135682651f133cf2a
Alex Boga <alecsboga@gmail.com>
2016-03-25 23:16:19 +0200

    added booking project

---
+++ booking/src/main/java/ro/sci/booking/Booking.java
@@ -0,0 +1,33 @@
+package ro.sci.booking;
+
+public class Booking {
+
+	private Accommodation room;
+	private BookingPeriod bookingPeriod;
+	// customer
+	// id
+	// price
+	// status
+
+	public Booking(Accommodation room, BookingPeriod bookingPeriod) {
+		this.setRoom(room);
+		this.setBookingPeriod(bookingPeriod);
+	}
+
+	public Accommodation getRoom() {
+		return room;
+	}
+
+	public void setRoom(Accommodation room) {
+		this.room = room;
+	}
+
+	public BookingPeriod getBookingPeriod() {
+		return bookingPeriod;
+	}
+
+	public void setBookingPeriod(BookingPeriod bookingPeriod) {
+		this.bookingPeriod = bookingPeriod;
+	}
+
+}
--- junitsci/src/main/java/ro/sci/math/Calculator.java
+++
@@ -1,9 +0,0 @@
-package ro.sci.math;
-
-public class Calculator {
-
-	public Object add(int i, int j) {
-		return i + j;
-	}
-
-}
